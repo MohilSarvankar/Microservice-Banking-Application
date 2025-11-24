70309f37dcfbbaa7025afd06958d65723b96dae0
Mohil Sarvankar <mohilsarvankar23@gmail.com>
2025-11-24 11:55:04 +0530

    Backend - Registry service (Eureka) created

---
+++ backend/registry-service/src/test/java/com/viper/registry/RegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viper.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
